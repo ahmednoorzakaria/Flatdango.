b456f765afef858b0ebdb9d52417957a01dd8a1b
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-30 09:23:51 +0300

    added a sidebar

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let showMovies = document.getElementById('Movies')
-    console.log(showMovies)
+document.addEventListener('DOMContentLoaded', function() {
+    let showMovies = document.getElementById('Movies');
+    let showList = document.getElementById('movie-list');
+  
     fetch('http://localhost:3000/films', {
-        method :'GET',
-        headers: {
-            'content-type':'application/json',
-            "Accept": "*application/json*"
-        } ,
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      }
     })
-    .then(res => res.json())
-    .then(movies => movieInputs(movies , showMovies))
-})
-
-function movieInputs(movies , showMovies){
+      .then(res => res.json())
+      .then(movies => {
+        movieInputs(movies, showMovies);
+        movieList(movies, showList);
+      })
+      .catch(error => console.error(error));
+  
+  });
+  
+  function movieInputs(movies, showMovies) {
     movies.forEach(movie => {
-        let movieWhere = document.createElement('div')
-        let All = `${movie.capacity}`
-        let sold = `${movie.tickets_sold}`
-        let Remaining = parseInt(All - sold)
-
-        console.log(Remaining)
-        movieWhere.innerHTML = `
+      let movieWhere = document.createElement('div');
+      let All = `${movie.capacity}`;
+      let sold = `${movie.tickets_sold}`;
+      let Remaining = parseInt(All - sold);
+  
+      movieWhere.innerHTML = `
         <img src='${movie.poster}'>
         <h3>${movie.title}<h3>
         <div id='Description'>
-        <p> ${movie.description}</p>
-        <p> <span> Runtime:${movie.runtime}</span> 
-            <span>Capacity:${movie.capacity}</span>
-            <span>Show Time : ${movie.showtime}</span>
-        </p>
-        <p>Tickets sold : ${movie.tickets_sold}</p>
-        <p>Remaining tickets : ${Remaining}</p>
-        <button> BOOK TICKET</button>
+          <p>${movie.description}</p>
+          <p>
+            <span>Runtime: ${movie.runtime} minutes</span>
+            <span>Capacity: ${movie.capacity}</span>
+            <span>Show Time: ${movie.showtime}</span>
+          </p>
+          <p>Tickets sold: ${movie.tickets_sold}</p>
+          <p>Remaining tickets: ${Remaining}</p>
+          <button>BOOK TICKET</button>
         </div>
-        `
-        showMovies.appendChild(movieWhere)
+      `;
+  
+      showMovies.appendChild(movieWhere);
     });
-}
+  }
+  
+  function movieList(movies, showList) {
+    movies.forEach(movie => {
+      let movieHere = document.createElement('li');
+      movieHere.innerHTML = movie.title;
+      showList.appendChild(movieHere);
+    });
+  }
+  
